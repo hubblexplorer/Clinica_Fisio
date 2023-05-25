c80cf93639396cbb04276972c96bfb2c4e10289a
RiseGhost <91985039+RiseGhost@users.noreply.github.com>
2023-05-25 13:02:25 +0100

    Add files via upload

---
+++ src/main/java/com/example/test_login2/repository/AgendaRepository.java
@@ -0,0 +1,13 @@
+package com.example.test_login2.repository;
+
+import com.example.test_login2.entity.Agenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+    List<Agenda> findByDate(java.sql.Date Date);
+    List<Agenda> findAgendaByDateBetween(java.sql.Date startDate, java.sql.Date endDate);
+}
